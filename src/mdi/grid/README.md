37b0da3efeb13672bc9c517dbb2f05a6c3bc8417
Austin Andrews <admin@templarian.com>
2020-04-12 18:29:48 -0500

    Basic icon grid rendering.

---
+++ src/mdi/grid/README.md
@@ -0,0 +1,24 @@
+# `<mdi-grid>`
+
+The `mdi-grid` component allows a standard way to render a grid of icons. Note this is setup for performance and does not reduce rendered nodes. So if 500 icons are passed the dom notes will stay for later renders.
+
+```typescript
+import '@mdi/web-component/mdiGrid.js';
+```
+
+```html
+<mdi-icon></mdi-icon>
+```
+
+| Attributes | Tested   | Description |
+| ---------- | -------- | ----------- |
+| icons      | &#x2705; | Set icon data |
+
+The minimal data set must contain the `name` and `data` field.
+
+```json
+[{
+  "name": "account",
+  "data": "..."
+}]
+```
--- src/mdi/grid/grid.ts
+++ src/mdi/grid/grid.ts
@@ -15,7 +15,43 @@ export default class MdiSearch extends HTMLElement {
 
   @Part() $grid: HTMLDivElement;
 
+  currentCount = 0;
+  items: [HTMLButtonElement, SVGPathElement][] = [];
+  svg = 'http://www.w3.org/2000/svg';
+
   render() {
-    // this.$path.setAttribute('d', this.path);
+    const count = this.icons.length;
+    // Render more grid items
+    for(let i = this.currentCount; i < count; i++) {
+      console.log('add item', i);
+      this.currentCount = i + 1;
+      const btn = document.createElement('button');
+      btn.addEventListener('click', () => {
+        console.log(this.icons[i]);
+      });
+      const svg = document.createElementNS(this.svg, 'svg');
+      svg.setAttribute('viewBox', '0 0 24 24');
+      const path = document.createElementNS(this.svg, 'path') as SVGPathElement;
+      svg.appendChild(path);
+      btn.appendChild(svg);
+      this.$grid.appendChild(btn);
+      this.items.push([btn, path]);
+    }
+    console.log('render grid', this.currentCount);
+    const columns = Math.floor(this.$grid.offsetWidth / 24);
+    this.$grid.style.gridTemplateColumns = `repeat(${columns}, 1.5rem)`;
+    const rows = Math.ceil(count / columns);
+    this.$grid.style.gridTemplateRows = `repeat(${rows}, 1.5rem)`;
+    this.items.forEach(([btn, path], i) => {
+      if (this.icons[i]) {
+        btn.style.gridColumn = `${(i % columns + 1)}`;
+        btn.style.gridRow = `${Math.ceil((i + 1) / columns)}`;
+        path.setAttribute('d', this.icons[i].data);
+        this.icons[i].id = i;
+      } else {
+        path.setAttribute('d', '');
+      }
+    });
+    this.$grid.style.height = `${1.5 * rows}rem`;
   }
 }
