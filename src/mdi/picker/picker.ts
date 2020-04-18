bc7e2956bec49d49551ca6140d1941cf3344cfde
Austin Andrews <admin@templarian.com>
2020-04-18 15:32:00 -0500

    More styles.

--- src/mdi/picker/picker.ts
+++ src/mdi/picker/picker.ts
@@ -1,13 +1,23 @@
-import { Component } from '@mdi/element';
+import { Component, Prop, Part } from '@mdi/element';
+import MdiButton from '../button/button';
+import { createPopper } from '@popperjs/core';
 
 import template from './picker.html';
 import style from './picker.css';
+
+window.process = { env: {} } as any;
 
 @Component({
   selector: 'mdi-picker',
   style,
   template
 })
-export default class MdiPicker extends HTMLElement {
+export default class MdiPicker extends MdiButton {
+  @Part() $popover: HTMLDivElement;
 
+  connectedCallback() {
+    createPopper(this.$button, this.$popover, {
+      placement: 'right-start',
+    });
+  }
 }
