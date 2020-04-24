e9d16cbee0ee5fd2a872660c8cb0d6e3dba228ee
Austin Andrews <admin@templarian.com>
2020-04-24 01:01:54 -0500

    Color picker.

--- src/mdi/color/color.ts
+++ src/mdi/color/color.ts
@@ -1,5 +1,6 @@
 import { Component, Part, Prop } from '@mdi/element';
 import { SWATCHES } from './constants';
+import { hexToRgb, normalizeHex } from './utils';
 
 import template from './color.html';
 import style from './color.css';
@@ -10,5 +11,74 @@ import style from './color.css';
   template
 })
 export default class MdiColor extends HTMLElement {
+  @Prop() value: string = '#000000';
 
+  @Part() $grid: HTMLDivElement;
+
+  buttons: [HTMLButtonElement, string, any][] = [];
+  isMouseDown: boolean = false;
+  index: number = -1;
+
+  connectedCallback() {
+    SWATCHES.forEach((group, groupIndex) => {
+      group.colors.forEach((color, colorIndex) => {
+        const button = document.createElement('button');
+        button.style.background = `#${color.hex}`;
+        if (color.name === 'black') {
+          button.style.gridArea = `13 / 17 / 14 / 19`;
+          button.classList.add('black');
+        } else if (color.name === 'white') {
+          button.style.gridArea = `11 / 17 / 12 / 19`;
+          button.classList.add('white');
+        } else {
+          button.style.gridRow = `${colorIndex + 1}`;
+          button.style.gridColumn = `${groupIndex + 1}`;
+        }
+        const index = this.buttons.push([
+          button,
+          `#${color.hex}`,
+          group
+        ]);
+        button.addEventListener('click', () => {
+          this.handleSelect(index - 1);
+        });
+        button.addEventListener('mouseenter', () => {
+          if (this.isMouseDown) {
+            this.handleSelect(index - 1);
+          }
+        });
+        this.$grid.appendChild(button);
+      });
+    });
+    this.$grid.addEventListener('mousedown', (e) => {
+      this.isMouseDown = true;
+      const index = this.buttons.findIndex(([b]) => b === e.target);
+      if (index !== -1) {
+        this.handleSelect(index);
+      }
+    });
+    const setToFalse = () => {
+      this.isMouseDown = false;
+      document.removeEventListener('mouseup', setToFalse);
+    };
+    document.addEventListener('mouseup', setToFalse);
+  }
+
+  handleSelect(index) {
+    const [button, hex, group] = this.buttons[index];
+    const rgb = hexToRgb(hex);
+    this.dispatchEvent(new CustomEvent('select', {
+      detail: {
+        group,
+        hex,
+        rgb
+      }
+    }));
+    if (this.index !== -1) {
+      const [lastButton] = this.buttons[this.index];
+      lastButton.classList.toggle('active', false);
+    }
+    button.classList.toggle('active', true);
+    this.index = index;
+  }
 }
