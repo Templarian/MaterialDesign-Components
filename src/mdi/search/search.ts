b9820be801fc46c2b117a318d9e06c0c144ac794
Austin Andrews <admin@templarian.com>
2020-05-25 16:54:55 -0500

    highlight

--- src/mdi/search/search.ts
+++ src/mdi/search/search.ts
@@ -6,6 +6,14 @@ import style from './search.css';
 
 const noIcon = 'M0 0h24v24H0V0zm2 2v20h20V2H2z';
 
+interface Item {
+  name: string,
+  type: string,
+  aliases: string[],
+  icon: string,
+  url: string
+}
+
 @Component({
   selector: 'mdi-search',
   style,
@@ -13,11 +21,13 @@ const noIcon = 'M0 0h24v24H0V0zm2 2v20h20V2H2z';
 })
 export default class MdiSearch extends HTMLElement {
   @Prop() path: string = noIcon;
+  @Prop() items: Item[] = [];
 
   @Part() $input: HTMLInputElement;
   @Part() $list: HTMLUListElement;
 
   isOpen: boolean = false;
+  term: string = '';
 
   connectedCallback() {
     this.$input.addEventListener('input', this.handleInput.bind(this));
@@ -27,12 +37,62 @@ export default class MdiSearch extends HTMLElement {
   handleInput(e) {
     const target = e.target as HTMLInputElement;
     const { value } = target;
-    console.log(value);
+    this.term = value;
+    this.updateList();
   }
 
   handleFocus() {
     this.isOpen = true;
     this.$list.style.display = 'block';
+  }
+
+  highlight(text: string) {
+    var i = 0;
+    var normalized = text;
+    var span = document.createElement('span');
+    if (this.term === '') {
+      span.innerText = text;
+      return span;
+    }
+    while (normalized) {
+      var index = normalized.toLowerCase().indexOf(this.term);
+      if (index === -1) {
+        const end = document.createElement('span');
+        end.innerText = normalized.substr(0, normalized.length);
+        span.appendChild(end);
+        normalized = '';
+      } else {
+        if (index > 0) {
+          const start = document.createElement('span');
+          start.innerText = normalized.substr(0, index);
+          span.appendChild(start);
+        }
+        const strong = document.createElement('strong');
+        strong.innerText = normalized.substr(index, this.term.length);
+        span.appendChild(strong);
+        normalized = normalized.substr(index + this.term.length, normalized.length);
+      }
+    }
+    return span;
+  }
+
+  clearList() {
+    while(this.$list.firstChild){
+      this.$list.removeChild(this.$list.firstChild);
+    }
+  }
+
+  updateList() {
+    this.clearList();
+    const termRegex = new RegExp(this.term, 'i');
+    this.items
+      .filter((item) => item.name.match(termRegex))
+      .forEach((item) => {
+        var li = document.createElement('li');
+        var text = this.highlight(item.name);
+        li.appendChild(text);
+        this.$list.appendChild(li);
+      });
   }
 
   render() {
