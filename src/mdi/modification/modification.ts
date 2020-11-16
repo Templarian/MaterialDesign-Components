3235aecc1a65c27d15d332ccc9e33979aed41e5f
Austin Andrews <admin@templarian.com>
2020-11-15 23:57:33 -0600

    Modification templates.

--- src/mdi/modification/modification.ts
+++ src/mdi/modification/modification.ts
@@ -9,7 +9,7 @@ import templateIconAliasCreated from './type/iconAliasCreated.html';
 import { Modification } from 'mdi/shared/models/modification';
 import { list, item } from './../shared/list';
 import { ModificationType } from 'mdi/shared/enums/modificationType';
-import { addTooltip } from './../tooltip/addTooltip';
+import { addTooltip, BOTTOM_END, BOTTOM_START } from './../tooltip';
 
 const noIcon = 'M0 0h24v24H0V0zm2 2v20h20V2H2z';
 const editIcon = 'M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z';
@@ -74,7 +74,13 @@ export default class MdiModification extends HTMLElement {
             if (issue) {
               addTooltip(issue, () => {
                 return `View on GitHub`;
-              }, 'left');
+              }, BOTTOM_END);
+            }
+            const avatar = n.querySelector('[part="avatar"]');
+            if (avatar) {
+              addTooltip(avatar, () => {
+                return modification.user.name;
+              }, BOTTOM_START);
             }
             return n;
           }
@@ -89,7 +95,7 @@ export default class MdiModification extends HTMLElement {
       );
     }
     if (changes.edit) {
-
+      this.$items.classList.toggle('edit', this.edit);
     }
   }
 
