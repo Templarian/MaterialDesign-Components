6cb5e4e5b7994587ff5799bd0164c1cb3d46057e
Austin Andrews <admin@templarian.com>
2021-02-07 13:53:10 -0600

    more examples

--- src/mdi/inputCheckList/__examples__/basic/basic.ts
+++ src/mdi/inputCheckList/__examples__/basic/basic.ts
@@ -1,11 +1,31 @@
 import { Component, Part, Prop } from '@mdi/element';
+import MdiInputCheckList from 'mdi/inputCheckList/inputCheckList';
 
 import template from './basic.html';
 
 @Component({
-  selector: 'x-annoy-basic',
+  selector: 'x-mdi-input-check-list-basic',
   template
 })
-export default class XAnnoyBasic extends HTMLElement {
+export default class XMdiInputCheckListBasic extends HTMLElement {
 
+  @Part() $input: MdiInputCheckList;
+  @Part() $value: HTMLSpanElement;
+
+  connectedCallback() {
+      this.$input.value = ['uuid1', 'uuid3'];
+      this.$input.options = [
+        { value: 'uuid1', label: 'Item 1' },
+        { value: 'uuid2', label: 'Item 2' },
+        { value: 'uuid3', label: 'Item 3', disabled: true },
+        { value: 'uuid4', label: 'Item 4' }
+      ];
+      this.$value.innerText = this.$input.value.join(',');
+      this.$input.addEventListener('change', this.handleChange.bind(this));
+  }
+
+  handleChange(e) {
+    const { value } = e.detail;
+    this.$value.innerText = value.join(',');
+  }
 }
