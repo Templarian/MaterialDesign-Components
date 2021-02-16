4891d87eee524bc351906c283c03bf7e8f941e73
Austin Andrews <admin@templarian.com>
2021-02-15 23:36:24 -0600

    mdiModalAlert basic example

---
+++ src/mdi/modalAlert/__examples__/basic/basic.ts
@@ -0,0 +1,29 @@
+import { Component, Part, Prop } from '@mdi/element';
+import MdiModalAlert from './../../../modalAlert/modalAlert';
+
+import template from './basic.html';
+
+@Component({
+  selector: 'x-mdi-modal-alert-basic',
+  template
+})
+export default class XMdiModalAlertBasic extends HTMLElement {
+
+  @Part() $button: HTMLButtonElement;
+  @Part() $result: HTMLSpanElement;
+
+  connectedCallback() {
+    this.$button.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  async handleClick() {
+    const result = await MdiModalAlert.open({
+      header: 'Delete Item',
+      message: 'Are you sure you want to delete the item?'
+    });
+    if (result) {
+      this.$result.innerText = result;
+    }
+  }
+
+}
